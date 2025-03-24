4b557583077ec3f79678ad73b998cce3d506e4de
AidenCMG <aiden.mckenzie@comcast.net>
2025-03-24 04:13:04 +0000

    Add Customer.java

---
+++ Customer.java
@@ -0,0 +1,32 @@
+class Customer extends User{
+    CheckingAccount checking = new CheckingAccount();
+    SavingsAccount savings = new SavingsAccount();
+    
+    public static void main(String[] args){
+
+    }
+
+    public Customer(){
+
+    }
+
+    public Customer(userName,PIN){
+
+    }
+
+    public void start(){
+
+    }
+
+    public String menu(){
+
+    }
+
+    public void changePin(){
+        
+    }
+
+    public String getReport(){
+        
+    }
+}
