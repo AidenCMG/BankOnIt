9de9d5bd9e661e4204708b4f558ab87dfe47145a
AidenCMG <aiden.mckenzie@comcast.net>
2025-03-25 15:15:45 +0000

    Add partial Customer.java

--- Customer.java
+++ Customer.java
@@ -3,27 +3,65 @@ class Customer extends User{
     SavingsAccount savings = new SavingsAccount();
     
     public static void main(String[] args){
+       Customer cu = new Customer();
+       cu.login(); 
 
     }
 
     public Customer(){
-
+        PIN = "0000";
+        userName = "Alice";
     }
 
     public Customer(userName,PIN){
-
+        this.setUserName(userName);
+        this.setPIN(PIN);
     }
 
     public void start(){
-
+        boolean keepGoing = true;
+        while(keepGoing){
+            String response = menu();
+            if(response.equals("1")){
+                System.out.println("Checking Account");
+                checking.start();
+            }
+            else if(response.equals("2")){
+                System.out.println("Savings Account");
+                savings.start();
+            }
+            else if(response.equals("3")){
+                System.out.println("Change PIN");
+                this.changePin();
+            }
+            else if(response.equals("0")){
+                keepGoing = false;
+            }
+            else {
+                System.out.println("Not a valid input");
+            }
     }
 
     public String menu(){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Customer Menu ");
+        System.out.println("0) Exit");
+        System.out.println("1) Manage Checking Account");
+        System.out.println("2) Manage Savings Account");
+        System.out.println("3) Change PIN");
+        return scanner.nextLine();
     }
 
     public void changePin(){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter current PIN: ");
+        if(scanner.nextLine().equals this.getPIN()){
+            System.out.println("Enter a new PIN");
+            this.setPIN(scanner.nextLine());
+        }
+        else{
+            System.out.println("Incorrect");
+        }
     }
 
     public String getReport(){
